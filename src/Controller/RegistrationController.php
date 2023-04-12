8c471b1906cac7a220b42364a5ae6063bd30c1b2
Theop <68582445+amounille@users.noreply.github.com>
2023-04-12 14:38:58 +0200

    Initial commit

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    /**
+     * @Route("/registration", name="user_register")
+     */
+    public function register(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Hasher le mot de passe avant de l'enregistrer dans la base de données
+            $password = $form->get('password')->getData();
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            $user->setPassword($hashedPassword);
+
+            // Enregistrer l'utilisateur dans la base de données
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('');
+        }
+
+        return $this->render('registration/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
